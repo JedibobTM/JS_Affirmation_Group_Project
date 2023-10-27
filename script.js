3f12163bc96222c8af65161c7402748b6f8c9483
Stephanie Hickman <stephick@gmail.com>
2023-10-27 11:02:11 -0500

    Completed required parts 1 & 2

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+function makeFire(){
+  let fire = document.getElementById("icyHot")
+  fire.innerHTML += "🔥"
+}
+
+function iceIceBaby(){
+  let ice = document.getElementById("icyHot")
+  ice.innerHTML += "❄️"
+}
+
+
+function handleSubmit(event){
+
+  event.preventDefault();
+
+  let tableBody = document.getElementById("tableBody")
+
+  let affirmationText = document.getElementById("affirmationText").value
+
+  let affirmationAuthor = document.getElementById("affirmationAuthor").value
+
+  tableBody.innerHTML += 
+  ` 
+  <tr>
+    <td>${affirmationText}</td>
+    <td>${affirmationAuthor}</td>
+  </tr>
+  `
+}
